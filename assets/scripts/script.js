ccdc6f1881b9a74a34176c459f3ac5dbfac1283f
Brandon Hukee <brandonhukee@Brandons-MacBook-Air.local>
2020-08-27 18:11:29 -0600

    Added sign up function
    Added a working sign up function and tested with a curl request

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+// const name = document.getElementById('name')
+// const password = document.getElementById('password')
+// const form = document.getElementById('form')
+// const errorElement = document.getElementById('error')
+//
+// form.addEventListener('Create Account', (e) => {
+//   let messages = []
+//   if (name.value === '' || name.value == null) {
+//     messages.push('Email is required')
+//   }
+//   if (messages.length > 0) {
+//     e.preventDefault()
+//     errorElement.innerText = messages.join(', ')
+//   }
+// })
