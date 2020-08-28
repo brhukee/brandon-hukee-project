378ebb363c84c03d625aa726fbef23349d4c0649
Brandon Hukee <brandonhukee@Brandons-MacBook-Air.local>
2020-08-28 15:30:10 -0600

    Fixed typos

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -1,14 +1,11 @@
-API="https://tic-tac-toe-api-development.herokuapp.com"
-URL_PATH="/sign-in"
-
-curl "${API}${URL_PATH}" \
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
 --include \
 --request POST \
   --header "Content-type: application/json" \
   --data '{
     "credentials": {
       "email": "'"${EMAIL}"'",
-      "password":  "'"${PASSWORD}"'",
+      "password":  "'"${PASSWORD}"'"
     }
   }'
 
--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,4 +1,4 @@
-curl ""https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
 --include \
 --request POST \
   --header "Content-type: application/json" \
