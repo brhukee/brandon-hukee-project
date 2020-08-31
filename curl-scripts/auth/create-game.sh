7ef40d2fb2ee0a177cd966fead72903120c8ea1c
Brandon Hukee <brandonhukee@Brandons-MacBook-Air.local>
2020-08-31 08:12:23 -0600

    Added create game
    Added create game curl script

--- curl-scripts/auth/create-game.sh
+++ curl-scripts/auth/create-game.sh
@@ -1,7 +1,7 @@
-curl "https://tic-tac-toe-api-production.herokuapp.com/games" \
+curl "https://tic-tac-toe-api-development.herokuapp.com/games" \
   --include \
   --request POST \
-  --header "Authorization: Bearer ${TOKEN}" \
   --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
 
 echo
